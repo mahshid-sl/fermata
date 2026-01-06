85337c791a5f27fb2ce7b1ae9789bec48d95f4fa
mahshid-sl <mahshiid.soleiman@gmail.com>
2026-01-06 21:46:40 +0330

    Refactor PopularDishes component to use FoodCard for rendering dishes and remove unused imports

--- components/shared/PopularDishes.tsx
+++ components/shared/PopularDishes.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { ChevronLeft, ChevronRight, Heart, Plus } from "lucide-react";
-import { Button } from "../ui/button";
-import { Card, CardContent } from "../ui/card";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Carousel from "./Carousel";
-import Image from "next/image";
-import StarRating from "./StarRating";
+import FoodCard from "./FoodCard";
 
 const mockFood = [
   {
@@ -60,105 +57,7 @@ export default function PopularDishes() {
       <Carousel
         items={mockFood}
         navigation={true}
-        renderItem={(dish) => (
-          <Card
-            className="
- group
-  h-full
-  min-h-90
-  rounded-3xl
-  overflow-hidden
-  border border-primary-200
-  bg-primary-50
-  transition
-  hover:shadow-xl
-
-"
-          >
-            <CardContent className="p-0 flex flex-col h-full">
-              {/* Image */}
-              <div className="relative  w-full aspect-4/3 overflow-hidden">
-                <button
-                  aria-label="افزودن به علاقه‌مندی‌ها"
-                  className="
-      absolute
-      top-3
-      left-3
-      z-10
-      flex
-      items-center
-      justify-center
-      w-9
-      h-9
-      rounded-full
-      bg-white/80
-      backdrop-blur
-      text-primary-600
-      hover:bg-primary-500
-      hover:text-white
-      transition
-      shadow-sm
-    "
-                >
-                  <Heart className="w-5 h-5  " />
-                </button>
-
-                <Image
-                  src={dish.image}
-                  alt={dish.name}
-                  fill
-                  className="
-          
-          object-contain
-          transition-transform
-          duration-300
-          group-hover:scale-105
-        "
-                />
-              </div>
-
-              {/* Content */}
-              <div className="flex flex-col flex-1 p-4 gap-5">
-                <div className="text-center">
-                  <h3 className="text-base font-bold text-primary-900 line-clamp-1 leading-10">
-                    {dish.name}
-                  </h3>
-                  <StarRating />
-                </div>
-
-                <p className="text-sm text-primary-700 line-clamp-2 text-center">
-                  {dish.description}
-                </p>
-
-                {/* Price + CTA */}
-                <div className="mt-auto flex items-center justify-between gap-2">
-                  <span className="flex items-center gap-1 text-primary-900 font-bold">
-                    {dish.price}
-                    <Image
-                      src="/toman.png"
-                      alt="toman"
-                      width={16}
-                      height={16}
-                    />
-                  </span>
-
-                  <Button
-                    size="sm"
-                    className="
-            rounded-full
-            px-4
-            bg-primary-500
-            hover:bg-primary-600
-          "
-                  >
-                    <Plus className="w-4 h-4 ml-1" />
-                    افزودن
-                  </Button>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-        )}
+        renderItem={(dish) => <FoodCard dish={dish} />}
       />
     </section>
   );
