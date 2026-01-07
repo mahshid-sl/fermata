f4871fac4006ae1590e02a4e3674c443e8632514
mahshid-sl <mahshiid.soleiman@gmail.com>
2026-01-08 01:00:43 +0330

    Add Footer component to RootLayout for consistent page structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 
 const vazirmatn = Vazirmatn({
   variable: "--font-vazirmatn",
@@ -30,6 +31,9 @@ export default function RootLayout({
           <Header />
         </header>
         <main> {children}</main>
+        <footer>
+          <Footer />
+        </footer>
       </body>
     </html>
   );
